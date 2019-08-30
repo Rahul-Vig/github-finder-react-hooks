7fdaccbbe0de09482f368768d7c0f60c0932cada
Rahul Vig <rahvee51395@gmail.com>
2019-08-29 20:13:31 -0400

    fixed alert bug

--- github-finder-react-hooks/src/App.js
+++ github-finder-react-hooks/src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/users/User";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+
 import "./App.css";
 
 const App = () => {
@@ -21,19 +22,11 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
-              <Route exact path="/about" component={About} />
-              <Route exact path="/user/:login" component={User} />
             </div>
             {/*you can pass in values for our properties, as shown here with title */}
           </div>
---
+++ github-finder-react-hooks/src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Nothing Found</h1>
+      <p className="lead">Page you are looking does not exist...</p>
+    </div>
+  );
+};
+
+export default NotFound;
